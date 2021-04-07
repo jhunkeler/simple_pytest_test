89e353eec4795e70172f977da06a724ed7199e78
Joseph Hunkeler <jhunkeler@gmail.com>
2021-04-07 17:00:41 -0400

    asdfasd

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -4,3 +4,6 @@ values = [(True) for x in range(20)]
 @pytest.mark.parametrize("test_truth", values)
 def test_all_true(test_truth):
     assert test_truth
+
+def test_death():
+    assert False, "kaboom"
